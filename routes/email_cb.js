2d65c9a576c6def850711e76ce9df7bab3c7e678
nonstar77 <genarubadra@gmail.com>
2023-10-10 10:58:36 +0700

    commit EE

--- routes/email_cb.js
+++ routes/email_cb.js
@@ -1,10 +1,8 @@
-// const email_cb = require('../controllers/email_cb');
-// const router = require('express').Router();
-const express = require('express');
-const router = express.Router();
-const controller = require("../controllers/index")
+// const express = require('express');
+// const router = express.Router();
+// const controller = require("../controllers/index")
 
-router.post('/add', controller.emailPost);
-router.post('/', controller.emailGet);
+// router.post('/add', controller.emailPost);
+// router.post('/', controller.emailGet);
 
-module.exports = router;
+// module.exports = router;
--- routes/user_review.js
+++ routes/user_review.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require("../controllers/index")
 
-router.post('/add', controller.userPost);
-router.get('/', controller.userGet);
+router.post('/addEmail', controller.emailPost);
+router.post('/getEmail', controller.emailGet);
+router.post('/addUser', controller.userPost);
+router.get('/getUser', controller.userGet);
 
 module.exports = router;
